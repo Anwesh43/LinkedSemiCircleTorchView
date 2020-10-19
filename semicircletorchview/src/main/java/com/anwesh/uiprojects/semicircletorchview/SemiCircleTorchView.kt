fcfb2306e8d8e0d4f506a9d6396f3b42269fccc6
Anwesh43 <anweshthecool0@gmail.com>
2020-10-19 16:42:09 +0530

    adding constants and libraries

--- semicircletorchview/src/main/java/com/anwesh/uiprojects/semicircletorchview/SemiCircleTorchView.kt
+++ semicircletorchview/src/main/java/com/anwesh/uiprojects/semicircletorchview/SemiCircleTorchView.kt
@@ -3,3 +3,29 @@ package com.anwesh.uiprojects.semicircletorchview
 /**
  * Created by anweshmishra on 19/10/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Path
+import android.app.Activity
+import android.content.Context
+
+val parts : Int =7
+val scGap : Float = 0.02f / parts
+val strokFactor : Float = 90f
+val sizeFactor : Float = 8.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#4CAF50",
+        "#3F51B5",
+        "#009688",
+        "#03A9F4"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val rot : Float = 90f
